c47cde082de5576e483d65575bac4f41bb4cdbd5
MissBizz <36085896+MissBizz@users.noreply.github.com>
2018-07-05 22:35:12 -0600

    Issue #11
    Changed how we deal with major force. We no longer unregister the event, but we not use a variable so major force is only looked at when warhorn is on

--- CanIHorn.lua
+++ CanIHorn.lua
@@ -20,43 +20,66 @@ end
 
 CAN_I_HORN = addon
 
+
+
 ----------------------------------------------------------
--- Main Functions  --
+-- Major Force Functions  --
 ----------------------------------------------------------
+
+--this sets horn to false, so that if a horn hasn't sounded - major force will be ignored.
+local WarhornActive = false
+
 local function WatchForce(_, changeType, _, effectName, unitTag, _, _, _, _, _, _, _, _, _, _, abilityId, _)
-    if changeType == EFFECT_RESULT_FADED then
-        CanIHornIndicatorText:SetColor(1, 1, 0, 1)
-        EVENT_MANAGER:UnregisterForEvent(forceevent, EVENT_EFFECT_CHANGED)
-    end
+    --if Warhorn hasn't sounded yet, WarhornActive will be false and therefore nothing will happen, lua assumes you mean you want the variable true
+    if WarhornActive then
+
+        --d(string.format("Passed warhorn true check"))
+
+        if changeType == EFFECT_RESULT_FADED then
+            CanIHornIndicatorText:SetColor(1, 1, 0, 1)
+            --this changed the text colour to yellow when major force fades
+            WarhornActive = false
+            --sets warhornActive back to false so other major forces don't change the colour
+        end
+   end
 end
 
-
-  local function RegisterForce()
-    EVENT_MANAGER:RegisterForEvent(forceevent, EVENT_EFFECT_CHANGED, WatchForce)
-    EVENT_MANAGER:AddFilterForEvent(forceevent, EVENT_EFFECT_CHANGED, REGISTER_FILTER_ABILITY_ID, 40225, REGISTER_FILTER_UNIT_TAG_PREFIX, "group")
-end
+----------------------------------------------------------
+-- Main Horn Functions  --
+----------------------------------------------------------
 
 -- (_, changeType, effectSlot, effectName, unitTag, beginTime, endTime, stackCount, iconName, buffType, effectType, abilityType, statusEffectType, unitName, unitID, abilityId, sourceUnitType)
+--This is our main function to change text when a warhorn fires or fades.
 local function IsHornOn(_, changeType, _, effectName, unitTag, _, _, _, _, _, _, _, _, _, _, abilityId, _)
 
     if changeType == EFFECT_RESULT_GAINED then
         --d(string.format("IsHornOn() gained effectName: %s, abilityId %s, unitTag %s", effectName, abilityId, unitTag))
         CanIHornIndicatorText:SetText("Warhorn is Active")
         CanIHornIndicatorText:SetColor(1, 0, 0, 1)
-        RegisterForce()
+            --checks only for aggressive horn, as that is the only time we care about major force
+            if abilityId == 40224 then
+                WarhornActive = true
+        --d(string.format("WarhornActive set to true"))
+                --sets WarhornActive to true to it will pass the check in the WatchForce function
+            end
         return
     end
 
     --d(string.format("IsHornOn() effectName: %s, abilityId %s, unitTag %s", effectName, abilityId, unitTag))
     CanIHornIndicatorText:SetText("Warhorn not Active")
     CanIHornIndicatorText:SetColor(0, 1, 0, 1)
+
     --d(string.format("IsHornOn() other effectName %s, abilityId %s, unitTag %s, changeType $s", effectName, abilityId, unitTag, changeType))
 end
 --------------------------------------------------
 -- Warhorn ID's  --
 ----------------------------------------------------------
-local hornID  = { [38564] = true, [46526] = true, [46528] = true, [46530] = true, [40224] = true, [46532] = true, [46535] = true, [46538] = true, [40221] = true, [46541] = true, [46544] = true, [46547] = true }
+local hornID  = { [38564] = true, [40224] = true, [40221] = true }
+--38564 is warhorn, 40224 aggressive, 40221 sturdy
 
+--------------------------------------------------
+-- Ability Event Registrations  --
+----------------------------------------------------------
 -- This makes sure we only run our IsHornOn function when it has to do with a warhorn ability ID (which we grab from the table above). That way IsHornOn doesn't run for every buff and ability ever
 local function RegisterFilterAbilities()
     local eventCounter = 0
@@ -68,7 +91,17 @@ local function RegisterFilterAbilities()
     end
 end
 
+--registers the Major Force buff specifically
+local function RegisterForce()
+    EVENT_MANAGER:RegisterForEvent(forceevent, EVENT_EFFECT_CHANGED, WatchForce)
+    EVENT_MANAGER:AddFilterForEvent(forceevent, EVENT_EFFECT_CHANGED, REGISTER_FILTER_ABILITY_ID, 40225, REGISTER_FILTER_UNIT_TAG_PREFIX, "group")
+end
 
+--------------------------------------------------
+-- Initial Horn Check  --
+----------------------------------------------------------
+
+--intial check for horn upon login
 local function CheckForHorn()
     local isActiveHorn = false
 
@@ -89,6 +122,7 @@ local function CheckForHorn()
     end
 end
 
+--waits until the player is activated to check for horn
 local function OnPlayerActivated()
     --d("it worked!")
     CheckForHorn()
@@ -99,6 +133,7 @@ end
 ----------------------------------------------------------
 -- Initialize Function  --
 ----------------------------------------------------------
+
 --The EVENT_ADD_ON_LOADED event fired, we made it past the check that it is indeed out addon on, so let's run this
 local function Initialize()
 
@@ -110,6 +145,7 @@ local function Initialize()
     addon.RestorePosition()
 
     RegisterFilterAbilities()
+    RegisterForce()
 
 
 end
